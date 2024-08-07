b12e719ce53660a5ef80a8db3a1e401b087ba7d9
Jérémy Thonon <124150582+JeremyThonon@users.noreply.github.com>
2024-08-07 22:04:20 +0200

    sync develop (#3)
    * init develop branch
    
    * feat: init design system (#2)
    
    * feat: init design system
    
    * feat: add font and init main modules
    
    * feat: init breakpoins service and menu mobile/desktop components
    
    * feat: init footer module
    
    * feat(footer): add footer text

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent implements OnInit {
+  isMobile: boolean = false;
+
+  constructor(private responsive: BreakpointObserver) { }
+
+  ngOnInit() { 
+    this.responsive.observe(Breakpoints.Handset)
+      .subscribe(result => {
+        if (result.matches) {
+          this.isMobile = result.matches;
+        } else {
+          this.isMobile = false;
+        }
+    });
+  }
+}
