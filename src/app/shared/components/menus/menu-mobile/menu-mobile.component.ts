4fea915b6cf30689fc6324790bc1a1b7fdd04129
Jérémy Thonon <124150582+JeremyThonon@users.noreply.github.com>
2024-08-07 22:02:05 +0200

    feat: init design system (#2)
    * feat: init design system
    
    * feat: add font and init main modules
    
    * feat: init breakpoins service and menu mobile/desktop components
    
    * feat: init footer module
    
    * feat(footer): add footer text

---
+++ src/app/shared/components/menus/menu-mobile/menu-mobile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-mobile',
+  templateUrl: './menu-mobile.component.html',
+  styleUrl: './menu-mobile.component.scss'
+})
+export class MenuMobileComponent {
+
+}
