8488a7d5f00781534a0d1e6849728be12091b2e1
JeremyThonon <jeremy.thonon@decathlon.com>
2024-08-16 08:36:21 +0200

    feat(content): init main content structure

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, MainComponent, FooterComponent],
   imports: [CommonModule, SharedModule],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, MainComponent, FooterComponent],
 })
 export class CoreModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './components/menus/menu/menu.component';
 import { MenuMobileComponent } from './components/menus/menu-mobile/menu-mobile.component';
 import { ButtonComponent } from './components/button/button.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
-  declarations: [MenuComponent, MenuMobileComponent, ButtonComponent],
+  declarations: [
+    MenuComponent,
+    MenuMobileComponent,
+    ButtonComponent,
+    ContentComponent,
+  ],
   imports: [CommonModule],
-  exports: [MenuComponent, MenuMobileComponent, ButtonComponent],
+  exports: [
+    MenuComponent,
+    MenuMobileComponent,
+    ButtonComponent,
+    ContentComponent,
+  ],
 })
 export class SharedModule {}
