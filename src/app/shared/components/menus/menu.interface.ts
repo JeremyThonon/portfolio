1a83608c4362161c3a8f4d8a41eaee2150abee3c
JeremyThonon <jeremy.thonon@decathlon.com>
2024-08-25 00:24:10 +0200

    feat(header): add header animation for desktop and mobile menus

--- src/app/shared/components/menus/menu.interface.ts
+++ src/app/shared/components/menus/menu.interface.ts
@@ -2,6 +2,7 @@ interface Link {
   index: string;
   label: string;
   id: string;
+  isMounted?: boolean;
 }
 
 export type { Link as LinkType };
--- src/app/shared/components/menus/menu/menu.component.ts
+++ src/app/shared/components/menus/menu/menu.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { links, scrollToContent } from '../menu.conf';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
+  animations: [
+    trigger('visibility', [
+      state(
+        'visible',
+        style({
+          transform: 'translateY(0)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'hidden',
+        style({
+          transform: 'translateY(-100%)',
+          opacity: 0,
+        })
+      ),
+      transition('visible => hidden', animate('250ms ease-out')),
+      transition('hidden => visible', animate('250ms ease-in')),
+    ]),
+  ],
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
   links = links;
   buttonLabel = 'Resume';
+  allLinksMounted = false;
 
   handleKeydown(event: KeyboardEvent): void {
     if (event.key === 'Enter' || event.key === ' ') {
@@ -19,4 +47,22 @@ export class MenuComponent {
   handleClick = (id: string): void => {
     scrollToContent(id);
   };
+
+  ngOnInit(): void {
+    let timeout = 300;
+
+    links.forEach(link => {
+      setTimeout(() => {
+        link.isMounted = true;
+
+        if (links[links.length - 1].isMounted) {
+          setTimeout(() => {
+            this.allLinksMounted = true;
+          }, 300);
+        }
+      }, timeout);
+
+      timeout += 300;
+    });
+  }
 }
