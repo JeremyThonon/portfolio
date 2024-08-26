5e6fb35bb8616b8b218e4a3cff3aba9152425ab6
JeremyThonon <jeremy.thonon@decathlon.com>
2024-08-26 21:03:41 +0200

    feat(global): bia1FEh7 - add back to top button

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmailBlockComponent } from './components/email-block/email-block.component';
 import { SocialsBlockComponent } from './components/socials-block/socials-block.component';
+import { BackToTopComponent } from './components/back-to-top/back-to-top.component';
 
 @NgModule({
-  declarations: [EmailBlockComponent, SocialsBlockComponent],
+  declarations: [
+    EmailBlockComponent,
+    SocialsBlockComponent,
+    BackToTopComponent,
+  ],
   imports: [CommonModule],
-  exports: [EmailBlockComponent, SocialsBlockComponent],
+  exports: [EmailBlockComponent, SocialsBlockComponent, BackToTopComponent],
 })
 export class FeaturesModule {}
